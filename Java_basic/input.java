7fc14e1a92adf88d66fefba0226eb7a764ee6abd
F. M Osman Faruq <155006600+fmosmanfaruq@users.noreply.github.com>
2025-02-02 18:35:23 +0600

    Add files via upload

---
+++ Java_basic/input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class input{
+    public static void main(String []args){
+        Scanner anything = new Scanner(System.in);
+        System.out.print("Enter a number ");
+        int n = anything.nextInt();
+        System.out.println("You have entered "+ n);
+    }
+}
