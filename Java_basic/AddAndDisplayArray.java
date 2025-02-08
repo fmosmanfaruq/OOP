7633660e4f32c05517e62161577cd4f592967bf4
F. M Osman Faruq <155006600+fmosmanfaruq@users.noreply.github.com>
2025-02-08 23:37:36 +0600

    Add files via upload

---
+++ Java_basic/AddAndDisplayArray.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class AddAndDisplayArray{
+	public static void main(String[] args){
+	ArrayList<Integer>any = new ArrayList<>();
+	any.add(10);
+	any.add(20);
+	any.add(30);
+	any.add(40);
+	any.add(50);
+System.out.println("Adding List of Array: "+any);
+}
+}
